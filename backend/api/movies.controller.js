87dfe974e155e642672fc5a70028e9234b8f060c
ricardotran92 <quanghung.850621@gmail.com>
2024-06-07 21:16:49 +0700

    Lab 3

--- backend/api/movies.controller.js
+++ backend/api/movies.controller.js
@@ -1,20 +1,22 @@
 // 22540008
-import MoviesDAO from '../dao/moviesDAO.js';
+import MoviesDAO from "../dao/moviesDAO.js";
 
 export default class MoviesController {
   static async apiGetMovies(req, res, next) {
-    const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20;
+    const moviesPerPage = req.query.moviesPerPage
+      ? parseInt(req.query.moviesPerPage)
+      : 20;
     const page = req.query.page ? parseInt(req.query.page) : 0;
     let filters = {};
     if (req.query.rated) {
       filters.rated = req.query.rated;
-    }
-    else if (req.query.title) {
+    } else if (req.query.title) {
       filters.title = req.query.title;
     }
     const { moviesList, totalNumMovies } = await MoviesDAO.getMovies({
-      filters, page,
-      moviesPerPage
+      filters,
+      page,
+      moviesPerPage,
     });
     let response = {
       movies: moviesList,
@@ -24,6 +26,31 @@ export default class MoviesController {
       total_results: totalNumMovies,
     };
     res.json(response);
+  }
+  // 22540008
+  static async apiGetMovieById(req, res, next) {
+    try {
+      let id = req.params.id || {};
+      let movie = await MoviesDAO.getMovieById(id);
+      if (!movie) {
+        res.status(404).json({ error: "not found" });
+        return;
+      }
+      res.json(movie);
+    } catch (e) {
+      console.log(`api, ${e}`);
+      res.status(500).json({ error: e });
+    }
+  }
+  // 22540008
+  static async apiGetRatings(req, res, next) {
+    try {
+      let propertyTypes = await MoviesDAO.getRatings();
+      res.json(propertyTypes);
+    } catch (e) {
+      console.log(`api,${e}`);
+      res.status(500).json({ error: e });
+    }
   }
 }
 
--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -1,43 +1,81 @@
 // 22540008
-let movies;
+import mongodb from "mongodb";
 
-export default class MoviesDAO{
-  static async injectDB(conn){
-    if(movies){
+let movies;
+const ObjectId = mongodb.ObjectId;
+
+export default class MoviesDAO {
+  static async injectDB(conn) {
+    if (movies) {
       return;
     }
-    try{
-      movies = await
-      conn.db(process.env.MOVIEREVIEWS_NS).collection('movies');
-    }
-    catch(e){
+    try {
+      movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
+    } catch (e) {
       console.error(`unable to connect in MoviesDAO: ${e}`);
     }
   }
   // 22540008
-  static async getMovies({// default filter
+  static async getMovies({
+    // default filter
     filters = null,
     page = 0,
     moviesPerPage = 20, // will only get 20 movies at once
-    } = {}){
-      let query;
-      if(filters){
-      if("title" in filters){
-      query = {$text: { $search: filters['title']}};
-      }else if("rated" in filters){
-        query = {"rated": { $eq: filters['rated']}}
-      } }
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      }
+    }
     let cursor;
-    try{
-      cursor = await movies.find(query).limit(moviesPerPage).skip(moviesPerPage * page);
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
       const moviesList = await cursor.toArray();
       const totalNumMovies = await movies.countDocuments(query);
-      return {moviesList, totalNumMovies};
-    }
-    catch(e){
+      return { moviesList, totalNumMovies };
+    } catch (e) {
       console.error(`Unable to issue find command, ${e}`);
-      return {moviesList:[], totalNumMovies: 0};
+      return { moviesList: [], totalNumMovies: 0 };
     }
+  }
+  // 22540008
+  static async getMovieById(id) {
+    try {
+      return await movies
+        .aggregate([
+          {
+            $match: { _id: new ObjectId(id) },
+          },
+          {
+            $lookup: {
+              from: "reviews",
+              localField: "_id",
+              foreignField: "movie_id",
+              as: "reviews",
+            },
+          },
+        ])
+        .next();
+    } catch (e) {
+      console.error(`something went wrong in getMovieById: ${e}`);
+      throw e;
     }
+  }
+  // 22540008
+  static async getRatings() {
+    let ratings = [];
+    try {
+      ratings = await movies.distinct("rated");
+      return ratings;
+    } catch (e) {
+      console.error(`unable to get ratings, $(e)`);
+      return ratings;
+    }
+  }
 }
-
